adb8ac85ed472740cbd9d6b8f3d7a68ef306a0c5
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-12-02 16:58:50 +0200

    fix: Fix naming, extract common logic

--- 2022/day-02/src/main.rs
+++ 2022/day-02/src/main.rs
@@ -8,69 +8,62 @@ fn main() {
     let mut args = env::args().skip(1);
     let filepath = args.next().unwrap_or(INPUT_PATH.to_string());
 
-    let score = get_score(&filepath);
-    let score_result = get_score_result(&filepath);
+    let score = get_score_from_plays(&filepath);
+    let score_result = get_score_from_result(&filepath);
     println!("Score: {}", score);
     println!("Score from result: {}", score_result);
 }
 
-fn get_score(path: &str) -> u32 {
-    let content = fs::read_to_string(path).expect("File should exist");
-    content
-        .split("\n")
-        .map(|x| x.trim())
-        .filter(|x| !x.is_empty())
-        .map(|x| x.parse::<Round>().expect(" "))
-        .collect::<Vec<Round>>()
-        .iter()
-        .map(|x| x.get_score())
-        .sum()
+fn get_score_from_plays(path: &str) -> u32 {
+    get_score(path, &|x: &str| Round::from_str_plays(x))
 }
 
-fn get_score_result(path: &str) -> u32 {
-    let content = fs::read_to_string(path).expect("File should exist");
-    content
+fn get_score_from_result(path: &str) -> u32 {
+    get_score(path, &|x: &str| Round::from_str_result(x))
+}
+
+fn get_score(path: &str, f: &dyn Fn(&str) -> Round) -> u32 {
+    fs::read_to_string(path)
+        .expect("File should exist")
         .split("\n")
         .map(|x| x.trim())
         .filter(|x| !x.is_empty())
-        .map(|x| Round::from_str_result(x))
-        .collect::<Vec<Round>>()
-        .iter()
+        .map(f)
         .map(|x| x.get_score())
         .sum()
 }
 
 struct Round {
-    first: Play,
-    second: Play,
+    opponent: Play,
+    player: Play,
     result: RoundResult,
 }
 
 impl Round {
-    fn new(first: Play, second: Play) -> Self {
-        let result = match (first, second) {
+    fn new(opponent: Play, player: Play) -> Self {
+        let result = match (opponent, player) {
             (Play::Rock, Play::Scissor)
             | (Play::Paper, Play::Rock)
             | (Play::Scissor, Play::Paper) => RoundResult::Lose,
-
-            (a, b) if a == b => RoundResult::Draw,
 
             (Play::Rock, Play::Paper)
             | (Play::Paper, Play::Scissor)
             | (Play::Scissor, Play::Rock) => RoundResult::Win,
 
-            _ => panic!("Unexpected case"),
+            (Play::Rock, Play::Rock)
+            | (Play::Paper, Play::Paper)
+            | (Play::Scissor, Play::Scissor) => RoundResult::Draw,
         };
 
         Round {
-            first,
-            second,
+            opponent,
+            player,
             result,
         }
     }
 
-    fn from_result(first: Play, result: RoundResult) -> Self {
-        let second = match (first, result) {
+    fn from_result(opponent: Play, result: RoundResult) -> Self {
+        let player = match (opponent, result) {
             (Play::Rock, r) => match r {
                 RoundResult::Win => Play::Paper,
                 RoundResult::Draw => Play::Rock,
@@ -89,22 +82,14 @@ impl Round {
         };
 
         Round {
-            first,
-            second,
+            opponent,
+            player,
             result,
         }
     }
 
-    fn from_str_result(input: &str) -> Self {
-        let mut args = input.split(" ");
-        let first = args.next().expect(" ").parse::<Play>().expect(" ");
-        let result = args.next().expect(" ").parse::<RoundResult>().expect(" ");
-
-        Self::from_result(first, result)
-    }
-
     fn get_score(&self) -> u32 {
-        let shape_score: u32 = match self.second {
+        let shape_score: u32 = match self.player {
             Play::Rock => 1,
             Play::Paper => 2,
             Play::Scissor => 3,
@@ -120,14 +105,21 @@ impl Round {
     }
 }
 
-impl FromStr for Round {
-    type Err = ();
-
-    fn from_str(input: &str) -> Result<Round, Self::Err> {
+impl Round {
+    fn from_str_result(input: &str) -> Self {
         let mut args = input.split(" ");
-        let first = args.next().expect(" ").parse::<Play>().expect(" ");
-        let second = args.next().expect(" ").parse::<Play>().expect(" ");
-        Ok(Round::new(first, second))
+        let opponent = args.next().expect(" ").parse::<Play>().expect(" ");
+        let result = args.next().expect(" ").parse::<RoundResult>().expect(" ");
+
+        Self::from_result(opponent, result)
+    }
+
+    fn from_str_plays(input: &str) -> Self {
+        let mut args = input.split(" ");
+        let opponent = args.next().expect(" ").parse::<Play>().expect(" ");
+        let player = args.next().expect(" ").parse::<Play>().expect(" ");
+
+        Self::new(opponent, player)
     }
 }
 
@@ -179,25 +171,25 @@ mod tests {
 
     #[test]
     fn test_sample() {
-        let result = get_score(TEST_PATH);
+        let result = get_score_from_plays(TEST_PATH);
         assert_eq!(result, 15);
     }
 
     #[test]
     fn test_input() {
-        let result = get_score(INPUT_PATH);
+        let result = get_score_from_plays(INPUT_PATH);
         assert_eq!(result, 17189);
     }
 
     #[test]
     fn test_result_sample() {
-        let result = get_score_result(TEST_PATH);
+        let result = get_score_from_result(TEST_PATH);
         assert_eq!(result, 12);
     }
 
     #[test]
     fn test_result_input() {
-        let result = get_score_result(INPUT_PATH);
+        let result = get_score_from_result(INPUT_PATH);
         assert_eq!(result, 13490);
     }
 }
